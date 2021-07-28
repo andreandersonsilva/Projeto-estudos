4db2040829709bc75ac29a1c76977d9f0fd3e64f
André Anderson Alves Silva <andre.anderson.silva@gmail.com>
2021-07-27 23:44:00 -0300

    Aulas de JS

---
+++ script.js
@@ -0,0 +1,47 @@
+let count = 0
+while(count < 5){
+    console.log(count)
+    count = count++
+}
+
+
+/*
+let idade = prompt("Qual a sua idade?")
+if(idade >= 18){
+    alert("Você é maior de idade!")
+}else{
+    alert("Você é menor de idade!")
+}
+*/
+
+
+
+
+//Dicionário com lista
+/*let frutas = [{nome: "maçã", cor: "Vermelha",}, {nome: "uva", cor: "Roxa",}]
+console.log(frutas[0].nome)
+alert(frutas[1].cor)
+*/
+
+//Dicionário
+/*let fruta = {nome: "maçã", cor: "Vermelha",}
+console.log(fruta.nome)
+alert(fruta.cor)
+*/
+
+/*alert("Teste\n Cheguei")
+prompt("Digite um número", "Aqui")
+confirm("Você deseja cessar a página?")
+
+document.write("Olá pessoal</br> Cheguei!!!")
+document.write("</br>Este é um texto teste para testar o que está sendo testado durante os testes")
+
+
+let lista = ["maçã", "feijão", "laranja"]
+console.log(lista[1])
+alert(lista[0])
+console.log(lista[2])
+//Modificando o item separador da lista
+console.log(lista.join(" - "))
+console.log(lista.join(" <> "))
+*/
